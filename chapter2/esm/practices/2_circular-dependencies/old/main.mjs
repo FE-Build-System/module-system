d2ed224749bc3b11f2db67665ae86b8c33d91bd4
bestseob93 <bestseob93@gmail.com>
2025-04-22 19:29:14 +0900

    madge 라이브러리 추가

--- chapter2/esm/practices/2_circular-dependencies/old/main.mjs
+++
@@ -1,4 +0,0 @@
-import { createUser } from "./userService.mjs";
-
-const newUser = createUser("Hackurity");
-console.log("Created user:", newUser);
--- chapter2/esm/practices/2_circular-dependencies/old/notificationService.mjs
+++
@@ -1,9 +0,0 @@
-import { getUserProfile } from "./userService.mjs"; // 여기서 순환 참조 발생
-
-export function sendWelcomeEmail(user) {
-  console.log(`Sending welcome email to ${user.name}`);
-  // 환영 이메일 전송 후, 사용자 프로필 정보를 추가로 조회
-  // 만약 userService의 getUserProfile이 아직 초기화되지 않았다면 에러 발생
-  const profile = getUserProfile(user.id);
-  console.log("Fetched user profile:", profile);
-}
