5380121486368dbaae88d1a07a0b06cc3d4e2a57
hackurity01 <hackurity01@gmail.com>
2025-05-07 23:15:38 +0900

    chapter3 init

---
+++ chapter3/1_packageJson/cjs_env/index.js
@@ -0,0 +1,6 @@
+import("./add.mjs").then((v) => {
+  console.log(v);
+});
+// const add = require("./add.mjs");
+
+// console.log("add", add);
---
+++ chapter3/1_packageJson/esm_env/index.js
@@ -0,0 +1,3 @@
+import { add } from "./add.cjs";
+
+console.log("add(10, 5) = ", add(10, 2));
