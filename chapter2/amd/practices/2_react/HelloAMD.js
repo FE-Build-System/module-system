07d60b0d2aad94fea9825842551a35f8c0846a9a
bestseob93 <bestseob93@gmail.com>
2025-05-18 21:22:00 +0900

    useEffect 예시 추가

--- chapter2/amd/practices/2_react/HelloAMD.js
+++ chapter2/amd/practices/2_react/HelloAMD.js
@@ -1,5 +1,9 @@
 define(['react'], function (React) {
   return function HelloAMD(props) {
+    React.useEffect(() => {
+      console.log('HelloAMD mounted')
+    }, [])
+
     return React.createElement('p', null, `Hello! ${props.message}`)
   }
 })
