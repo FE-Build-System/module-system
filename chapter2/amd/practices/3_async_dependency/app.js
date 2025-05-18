ec955ba5ca3ee423f92a78bff8d70edc5f4718cf
bestseob93 <bestseob93@gmail.com>
2025-05-18 22:10:04 +0900

    remove comments

--- chapter2/amd/practices/3_async_dependency/app.js
+++ chapter2/amd/practices/3_async_dependency/app.js
@@ -5,11 +5,9 @@ require.config({
   },
 });
 
-// shim 설정 없이 로드
 require(["jquery", "jquery-color"], function ($) {
   console.log("jQuery 및 color 플러그인 로드 시도");
 
-  // 테스트: 배경색을 빨간색으로 애니메이션
   $("body")
     .css("backgroundColor", "#fff")
     .animate({ backgroundColor: "#f00" }, 1000);
