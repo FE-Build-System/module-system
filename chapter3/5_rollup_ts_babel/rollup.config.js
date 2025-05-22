f231c32c1bc356602624a30599720c31308bc5be
hackurity01 <hackurity01@gmail.com>
2025-05-22 20:38:02 +0900

    rollup

--- chapter3/5_rollup_ts_babel/rollup.config.js
+++ chapter3/5_rollup_ts_babel/rollup.config.js
@@ -1,7 +1,24 @@
+import commonjs from "@rollup/plugin-commonjs";
+
 export default {
-  input: "src/index.js",
+  // input: "src/esm_index.mjs",
+  input: "src/cjs_index.js",
   output: {
-    file: "dist/bundle.cjs.js",
-    format: "cjs",
+    dir: "dist",
+    entryFileNames: "[name]_bundle.js",
+    format: "es",
   },
+  plugins: [
+    // 1) node_modules 패키지 해석
+    // resolve({
+    //   extensions: [".js", ".cjs"], // .cjs 파일도 해석
+    //   browser: true, // 브라우저용 진입점을 우선
+    // }),
+    // 2) CJS → ESM 변환
+    // commonjs({
+    //   extensions: [".js", ".cjs"], // 처리할 확장자
+    //   transformMixedEsModules: true, // ES+CommonJS 혼합 파일도 변환
+    //   requireReturnsDefault: "auto", // default 내보내기 자동 감지
+    // }),
+  ],
 };
