9b1de7442147b59dbbd7f969ba68bce9a47fa1cd
hackurity01 <hackurity01@gmail.com>
2025-05-25 23:14:17 +0900

    eslint

--- chapter3/7_eslint/v9/eslint.config.mjs
+++ chapter3/7_eslint/v9/eslint.config.mjs
@@ -1,14 +1,28 @@
+import { defineConfig } from "eslint/config";
 import globals from "globals";
+import js from "@eslint/js";
 
-export default [
+export default defineConfig([
   {
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.cjs"],
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+      globals: {
+        ...globals.node,
+        ...globals.es2021,
+      },
+    },
+  },
+  {
+    files: ["**/*.{js,mjs}"],
+    languageOptions: {
       globals: {
         ...globals.browser,
         ...globals.es2021,
       },
     },
   },
-];
+]);
