d5334b71a482c07de54033736e082723aef1c301
bestseob93 <bestseob93@gmail.com>
2025-05-25 23:01:44 +0900

    eslint v9 config 수정

--- chapter3/7_eslint/v9/eslint.config.mjs
+++ chapter3/7_eslint/v9/eslint.config.mjs
@@ -1,48 +1,14 @@
-import js from '@eslint/js';
+import globals from "globals";
 
 export default [
   {
-    ignores: ['node_modules/**'],
-  },
-  {
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'script',
-      globals: js.configs.recommended.languageOptions.globals,
-    },
-    linterOptions: {
-      reportUnusedDisableDirectives: true,
-    },
-    rules: js.configs.recommended.rules,
-  },
-  {
-    files: ['*.mjs'],
-    languageOptions: {
-      sourceType: 'module',
-      ecmaVersion: 'latest',
-    },
-    env: {
-      browser: true,
-    },
-  },
-  {
-    files: ['*.cjs'],
-    languageOptions: {
-      sourceType: 'script',
-      ecmaVersion: 'latest',
-    },
-    env: {
-      node: true,
-    },
-  },
-  {
-    files: ['./amd/**/*.js'],
-    languageOptions: {
-      sourceType: 'script',
-      ecmaVersion: 'latest',
-    },
-    env: {
-      amd: true,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+      },
     },
   },
 ];
