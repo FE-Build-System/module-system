63f043c8c2195d943420bc48236d6d22f8690a8a
bestseob93 <bestseob93@gmail.com>
2025-06-11 21:27:27 +0900

    jquery-color 추가

--- chapter2/amd/practices/1_base/js/app/lib.js
+++ chapter2/amd/practices/1_base/js/app/lib.js
@@ -1,6 +1,7 @@
-define(['jquery'], function ($) {
+define(['jquery', 'jquery-color'], function ($) {
   return {
     getBody: function () {
+      $("body").css("backgroundColor", "#fff").animate({ backgroundColor: "#f00" }, 1000);
       return $('body');
     }
   }
